fff08d82589183f2f91fd00dbf98d2163d928a32
Nadav Benedek <nadavbenedek@gmail.com>
2016-01-19 22:07:46 +0200

    Allow specific handling of routes

--- lib/htmlcompressor/rack.rb
+++ lib/htmlcompressor/rack.rb
@@ -1,41 +1,54 @@
+# patch for rack.rb of HtmlCompressor
 module HtmlCompressor
 
   class Rack
 
     DEFAULT_OPTIONS = {
-      :enabled => true,
-      :remove_multi_spaces => true,
-      :remove_comments => true,
-      :remove_intertag_spaces => false,
-      :remove_quotes => true,
-      :compress_css => false,
-      :compress_javascript => false,
-      :simple_doctype => false,
-      :remove_script_attributes => true,
-      :remove_style_attributes => true,
-      :remove_link_attributes => true,
-      :remove_form_attributes => false,
-      :remove_input_attributes => true,
-      :remove_javascript_protocol => true,
-      :remove_http_protocol => false,
-      :remove_https_protocol => false,
-      :preserve_line_breaks => false,
-      :simple_boolean_attributes => true
+        :enabled => true,
+        :remove_multi_spaces => true,
+        :remove_comments => true,
+        :remove_intertag_spaces => false,
+        :remove_quotes => true,
+        :compress_css => false,
+        :compress_javascript => false,
+        :simple_doctype => false,
+        :remove_script_attributes => true,
+        :remove_style_attributes => true,
+        :remove_link_attributes => true,
+        :remove_form_attributes => false,
+        :remove_input_attributes => true,
+        :remove_javascript_protocol => true,
+        :remove_http_protocol => false,
+        :remove_https_protocol => false,
+        :preserve_line_breaks => false,
+        :simple_boolean_attributes => true,
+        :compress_by_default => true
     }
 
     def initialize app, options = {}
       @app = app
 
-      options = DEFAULT_OPTIONS.merge(options)
+      @options = DEFAULT_OPTIONS.merge(options)
 
-      @compressor = HtmlCompressor::Compressor.new(options)
+      @compressor = HtmlCompressor::Compressor.new(@options)
 
     end
 
     def call env
       status, headers, body = @app.call(env)
 
-      if headers.key? 'Content-Type' and headers['Content-Type'] =~ /httml/
+      path = Rails.application.routes.recognize_path(env['PATH_INFO'])
+      compress_info = path[:compress]
+      should_we_compress = @options[:compress_by_default]
+      if (compress_info == true)
+        should_we_compress = true
+      elsif (compress_info == false)
+        should_we_compress = false
+      else (compress_info == nil)
+        # Do nothing, leave default
+      end
+
+      if headers.key? 'Content-Type' and headers['Content-Type'] =~ /html/ and should_we_compress
         content = ''
 
         body.each do |part|
